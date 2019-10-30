5b76f59b0359dfe4cd3af6deb11a59d0c0fe4ff0
40823219 <a0976682879@gmail.com>
2019-10-31 00:26:50 +0800

    01

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'http://mde.tw/cp2019 \xa0 \n https://dartpad.dev?id=c886fed4f87f8814ea51fa1cbb175e35 \xa0 \n \n \n', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '課程網站: http://mde.tw/cp2019 \xa0 \n 我的倉儲: https://github.com/40823219/cp2019\xa0 \xa0 \n \n', 'tags': '', 'url': 'About.html'}, {'title': '每週進度', 'text': 'http://mde.tw/cp2019 \xa0 \n https://dartpad.dev?id=c886fed4f87f8814ea51fa1cbb175e35 \xa0 \n \n \n', 'tags': '', 'url': '每週進度.html'}]};
